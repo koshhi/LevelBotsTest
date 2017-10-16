8beb4118569d8b86e2a2ca2de002e7fd2e4df286
Cesar Alvarez Marin <cesaralvarezmarin@gmail.com>
2017-10-17 01:10:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
     port : process.env.PORT || 8080,
-    db : process.env.MONGODB_URI || 'mongodb://localhost:27017/companiesApp',
+    db : process.env.MONGODB_URI || 'heroku_rr4lv4n0:bka57saoo9pb9m2on95bnp48al@ds121965.mlab.com:21965/heroku_rr4lv4n0',
     SECRET_TOKEN: 'levelbotskeytoken'
 };
