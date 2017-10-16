f6ea4ac64a1a4238ac8a3afcf334f0102eea8954
koshhi <cesaralvarezmarin@gmail.com>
2017-10-16 23:10:49 +0200

    mongo db uri added

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
     port : process.env.PORT || 8080,
-    db : process.env.MONGODB || 'mongodb://localhost:27017/companiesApp',
+    db : process.env.MONGODB_URI || 'mongodb://localhost:27017/companiesApp',
     SECRET_TOKEN: 'levelbotskeytoken'
 };
